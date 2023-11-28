d74c7ad54053b393a51006990cecaa3a50ebbcb8
Siberuz <ruzz.arianto@gmail.com>
2023-11-29 02:03:06 +0700

    update js
    coba bikin component dan class

---
+++ class/solarSystem.js
@@ -0,0 +1,36 @@
+import * as THREE from "../three.js/build/three.module.js";
+import { OrbitControls } from "../three.js/examples/jsm/controls/OrbitControls.js";
+
+export class SolarSystem {
+  constructor(containerId) {
+    this.container = document.getElementById(containerId);
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    window.addEventListener("resize", () => this.handleWindowResize());
+
+    this.camera.position.z = 3;
+  }
+
+  handleWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  animate() {
+    requestAnimationFrame(() => this.animate());
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+  }
+}
---
+++ component/starsComponent.js
@@ -0,0 +1,24 @@
+import * as THREE from "../three.js/build/three.module.js";
+export function starsFunction(scene) {
+  const starsGeometry = new THREE.BufferGeometry();
+  const starsMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
+    size: 0.3,
+  });
+
+  const starsVertices = [];
+  for (let i = 0; i < 10000; i++) {
+    const x = (Math.random() - 0.5) * 2000;
+    const y = (Math.random() - 0.5) * 2000;
+    const z = (Math.random() - 0.5) * 2000;
+    starsVertices.push(x, y, z);
+  }
+
+  starsGeometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(starsVertices, 3)
+  );
+
+  const stars = new THREE.Points(starsGeometry, starsMaterial);
+  scene.add(stars);
+}
