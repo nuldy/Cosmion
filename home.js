ac0d9746365e1cd123b193744ebd6538795b77b5
Dimas <sebastianus.anjangasmara@binus.ac.id>
2023-12-04 22:53:27 +0700

    updateHome

--- home.js
+++ home.js
@@ -2,9 +2,10 @@ import * as THREE from "./three.js/build/three.module.js";
 import { SolarSystem } from "./class/solarSystem.js";
 import { starsFunction } from "./component/starsComponent.js";
 
-const solarSystem = new SolarSystem("bg-Home");
+const solarSystem = new SolarSystem("bg-home");
 
 starsFunction(solarSystem.scene);
 solarSystem.animate();
+
 
 
